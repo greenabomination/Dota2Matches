4095c6d0bf977170be9470d718ab3dabdd7de55d
greenabomination <greenabomination@gmail.com>
2015-03-16 15:44:22 +0500

    post-parsing JSON

--- app/src/main/java/com/greenapp/dota2matches/Match.java
+++ app/src/main/java/com/greenapp/dota2matches/Match.java
@@ -1,8 +1,5 @@
 package com.greenapp.dota2matches;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Created by herroino on 14.03.2015.
  * класс для хранения матчей
@@ -11,42 +8,35 @@ public class Match {
     private int match_id;
     private String team1;
     private String team2;
-    private Date datetime;
-    private int status;
-    private String league;
-    private String link;
-    private int winner;
+    private int league_id;
+
 
     // конструктор
     public Match(
             int _match_id,
             String _team1,
             String _team2,
-            Date _datetime,
-            int _status,
-            String _league,
-            String _link,
-            int _winner
+            int _league_id
     ) {
+        if (_team1.length() == 0) {
+            team1 = "unknown";
+        } else {
+            team1 = _team1;
+        }
+        if (_team2.length() == 0) {
+            team2 = "unknown";
+        } else {
+            team2 = _team2;
+        }
         match_id = _match_id;
-        team1 = _team1;
-        team2 = _team2;
-        datetime = _datetime;
-        status = _status;
-        league = _league;
-        link = _link;
-        winner = _winner;
+        league_id = _league_id;
     }
 
     //метод возвращает суммарно данные по матчу
-    public String getSummary() {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        String dateString = sdf.format(datetime);
-        if (winner == 0) {
-            return dateString + ": " + team1 + " vs " + team2;
-        } else {
-            return dateString + ": " + team1 + " vs " + team2 + " (winner:" + winner + ")";
-        }
+    public String toString() {
+
+        return match_id + ": " + team1 + " vs " + team2 + " (league:" + league_id + ")";
+
     }
 
 }
--- app/src/main/java/com/greenapp/dota2matches/MatchesListFragment.java
+++ app/src/main/java/com/greenapp/dota2matches/MatchesListFragment.java
@@ -2,7 +2,10 @@ package com.greenapp.dota2matches;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 
 import java.io.IOException;
@@ -13,6 +16,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by herroino on 14.03.2015.
@@ -20,6 +25,8 @@ import java.util.ArrayList;
  */
 public class MatchesListFragment extends ListFragment {
     private static final String TAG = "DOTA2";
+
+    private Handler handler = new Handler();
     //адаптер для списка
     ArrayAdapter<Match> aa;
 
@@ -29,7 +36,7 @@ public class MatchesListFragment extends ListFragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //выбираем стайл для списка из стандартных лэйаутов
-        int layout_id = android.R.layout.simple_list_item_2;
+        int layout_id = android.R.layout.simple_list_item_1;
         //создаем аррейадаптер
         aa = new ArrayAdapter<Match>(getActivity(), layout_id, matches);
         //назначаем списку адаптер
@@ -61,8 +68,15 @@ public class MatchesListFragment extends ListFragment {
                 InputStream inputStream = connection.getInputStream();
                 //так как это джисон, то с этого момента все будет не как парсинг сайта
                 JsonReader reader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
-
-
+                //и тут начинается истинная магия
+                reader.beginObject();
+                Log.d(TAG, reader.nextName());
+                reader.beginObject();
+                Log.d(TAG, reader.nextName());
+                readMatchesArray(reader);
+                Log.d(TAG, reader.nextName() + " : " + reader.nextInt());
+                reader.endObject();
+                reader.endObject();
             }
 
 
@@ -75,9 +89,108 @@ public class MatchesListFragment extends ListFragment {
         }
     }
 
+    public void readMatchesArray(JsonReader r) throws IOException {
+        List mat = new ArrayList();
+        r.beginArray();
+        while (r.hasNext()) {
+            getGameObject(r);
+        }
+        r.endArray();
+    }
+
+    public void getGameObject(JsonReader r) throws IOException {
+        int match_id = -1;
+        int league_id = -1;
+        String team1 = "", team2 = "";
+// пробегаемся по игрокам
+        r.beginObject();
+        Log.d(TAG, r.nextName());
+        r.beginArray();
+        while (r.hasNext()) {
+            readPlayerData(r);
+        }
+        r.endArray();
+        //получаем
+        while (r.hasNext() && r.peek() == JsonToken.NAME) {
+            String name = r.nextName();
+            if (name.equals("match_id")) {
+                match_id = r.nextInt();
+            } else if (name.equals("league_id")) {
+                league_id = r.nextInt();
+            } else if (name.equals("radiant_team")) {
+                team1 = getTeamInfo(r, "radiant_team");
+            } else if (name.equals("dire_team")) {
+                team2 = getTeamInfo(r, "dire_team");
+            } else {
+                r.skipValue();
+            }
+        }
+        final Match match = new Match(match_id, team1, team2, league_id);
+        handler.post(new Runnable() {
+            public void run() {
+                addNewMatch(match);
+            }
+
+        });
+        r.endObject();
+    }
+
+    public void readPlayerData(JsonReader r) throws IOException
+
+    {
+        int account_id = 0;
+        String player_name = "";
+        int hero_id = 0;
+        int team = -1;
+
+        r.beginObject();
+        while (r.hasNext()) {
+            String name = r.nextName();
+            if (name.equals("account_id")) {
+                account_id = r.nextInt();
+            } else if (name.equals("name")) {
+                player_name = r.nextString();
+            } else if (name.equals("hero_id")) {
+                hero_id = r.nextInt();
+            } else if (name.equals("team")) {
+                team = r.nextInt();
+            } else {
+                r.skipValue();
+            }
+        }
+        Log.d(TAG, "player: " + player_name + " (" + account_id + ") hero - " + hero_id + "; team - " + team);
+        r.endObject();
+    }
+
+    public String getTeamInfo(JsonReader r, String t) throws IOException {
+        String team_name = "";
+        int team_id = -1;
+        long team_logo = 0;
+        boolean complete = false;
+
+        r.beginObject();
+        while (r.hasNext()) {
+            String name = r.nextName();
+            if (name.equals("team_name")) {
+                team_name = r.nextString();
+            } else if (name.equals("team_id")) {
+                team_id = r.nextInt();
+            } else if (name.equals("team_logo")) {
+                team_logo = r.nextLong();
+            } else if (name.equals("complete")) {
+                complete = r.nextBoolean();
+            } else {
+                r.skipValue();
+            }
+        }
+        r.endObject();
+        Log.d(TAG, t + " - " + team_name);
+        return team_name;
+    }
 
     private void addNewMatch(Match m) {
-
+        matches.add(m);
+        aa.notifyDataSetChanged();
     }
 
 }
